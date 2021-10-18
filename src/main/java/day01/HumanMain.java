f03f5b3cc49480f2fdd1a9e18931c14451232a2e
karikasg <karikas.gabor@gmail.com>
2021-10-18 15:58:57 +0200

    Day01 konzultáció 1. feladat

---
+++ src/main/java/day01/HumanMain.java
@@ -0,0 +1,22 @@
+package day01;
+
+import java.util.Scanner;
+
+public class HumanMain {
+
+    public static void main(String[] args) {
+        String name;
+        int age;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Kérem a nevet: ");
+        name = scanner.nextLine();
+        System.out.print("Kérem az életkort: ");
+        age = scanner.nextInt();
+
+        Human human = new Human(name, age);
+
+        System.out.println("A személy adatai:  név: "+human.getName()+", életkora: "+human.getAge());
+    }
+}
